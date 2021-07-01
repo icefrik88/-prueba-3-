bf9cb66a39f0e9cf191155a37183b833b3217c3f
roGhirardelli <ro.ghirardelli@duocuc.cl>
2021-07-01 16:30:00 -0400

    Token (enviado por Marcelo)

--- biblioteca_site/website/rest_libro/views.py
+++ biblioteca_site/website/rest_libro/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from rest_framework import serializers, status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from mod1.models import Libro
 from .serializers import LibroSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 @csrf_exempt
 @api_view(['GET', 'POST'])
+@permission_classes((IsAuthenticated,))
 def lista_libros(request):
     if request.method == 'GET':
         libro = Libro.objects.all()
@@ -24,6 +27,7 @@ def lista_libros(request):
             return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes((IsAuthenticated,))
 def detalle_libro(request, id):
     try:
         libro = Libro.objects.get(ISBN=id)
--- biblioteca_site/website/website/settings.py
+++ biblioteca_site/website/website/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'mod1',
     'rest_libro',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK ={
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
